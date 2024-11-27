0d61d4f404cd5309601b3f8f75df2a3f589319cb
Nishanth T <t.nishanth98@gmail.com>
2024-11-27 17:13:57 +0530

    APP_KEY and CLIENT_KEY removed

--- lib/back4app_db.dart
+++ lib/back4app_db.dart
@@ -4,7 +4,7 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 class Back4App {
 
     static Future<void> connectToDB() async {
-        const keyApplicationId = '<YOUR_APP_ID';
+        const keyApplicationId = '<YOUR_APP_ID>';
         const keyClientKey = '<YOUR_CLIENT_KEY>';
         const keyParseServerUrl = 'https://parseapi.back4app.com';
 
